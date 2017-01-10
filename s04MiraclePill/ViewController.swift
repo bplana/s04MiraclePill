8a9b8e107ac35596a3e22ba4b914761221edb5cf
BP <oh_strawberry@yahoo.com>
2017-01-10 15:47:39 -0800

    added labels and text fields

--- s04MiraclePill/ViewController.swift
+++ s04MiraclePill/ViewController.swift
@@ -8,14 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    // UIPICKER -- DON'T FORGET PROTOCOLS! (Above)
+    
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    // So we can change the text on it
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let states = ["Alaska",
+                  "Alabama",
+                  "Arkansas",
+                  "American Samoa",
+                  "Arizona",
+                  "California",
+                  "Colorado",
+                  "Connecticut",
+                  "District of Columbia",
+                  "Delaware",
+                  "Florida",
+                  "Georgia",
+                  "Guam",
+                  "Hawaii",
+                  "Iowa",
+                  "Idaho",
+                  "Illinois",
+                  "Indiana",
+                  "Kansas",
+                  "Kentucky",
+                  "Louisiana",
+                  "Massachusetts",
+                  "Maryland",
+                  "Maine",
+                  "Michigan",
+                  "Minnesota",
+                  "Missouri",
+                  "Mississippi",
+                  "Montana",
+                  "North Carolina",
+                  "North Dakota",
+                  "Nebraska",
+                  "New Hampshire",
+                  "New Jersey",
+                  "New Mexico",
+                  "Nevada",
+                  "New York",
+                  "Ohio",
+                  "Oklahoma",
+                  "Oregon",
+                  "Pennsylvania",
+                  "Puerto Rico",
+                  "Rhode Island",
+                  "South Carolina",
+                  "South Dakota",
+                  "Tennessee",
+                  "Texas",
+                  "Utah",
+                  "Virginia",
+                  "Virgin Islands",
+                  "Vermont",
+                  "Washington",
+                  "Wisconsin",
+                  "West Virginia",
+                  "Wyoming"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
 //        self.view.backgroundColor = UIColor.purple
         
-        
+        statePicker.dataSource = self
+        statePicker.delegate = self
     
     }
 
@@ -24,6 +88,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // To open the picker
+    @IBAction func stateBtnPressed(_ sender: Any) {
+    }
 
+    // Next 3 below -- Implement required functions (for UIPicker protocols)
+    // numberOfComponents / numberOfRowsInComponent / titleForRow
+    
+    // "return 0" for now (at the beginning)
+    // Like a column
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // "return 0" for now
+    // Based on the # of US states
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    // "return "" <empty string>" for now
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
 }
 
